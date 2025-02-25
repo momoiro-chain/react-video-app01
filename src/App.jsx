bd29fd53b837d980d5896dfb1bdc175d0f2a7312
momoiro-chain <y.horiguchi.tf@gmail.com>
2025-02-25 23:59:11 +0900

    主にCSVの出力系を追記

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,62 @@ function App() {
    );
   };
 
+
+
+
   // CSVの出力
   const handleExportCSV = () => {
+    if(clickData.length === 0) {
+      alert("出力するデータがありません。")
+      return;
+    }
 
+    const header = ["No.", "X座標", "Y座標","時間(sec)","補足",""] // headerとしてCSVの一番上の行を手書き
+    const csvContent = [header]; // ここでheaderをリストに入れといて、この下で他の物を追加するコードを記述
+    clickData.forEach((item) => {
+      csvContent.push([
+        item.id,
+        item.x,
+        item.y,
+        item.time.toFixed(3),
+        item.note || "" // noteがなければ空文字
+      ]);
+    });
+    const csv = csvContent
+    .map((row) => 
+      row
+        .map((cell) => {
+          const value = String(cell);
+          return value.includes(",") || value.includes('"') // これらの文字をどっちか含んでたらreplaceする処理に行く
+          ? `"${value.replace(/"/g,'""')}"` // CSVのエスケープへの対応
+          : value; // 上のやつ含まなければvalueをそのまま返す
+        })
+        .join(",")
+    )
+    .join("\n"); // 行を変えるための改行。これと,のjoinのために2回のmap.
+    const bom = new Uint8Array([0xEF, 0xBB, 0xBF]); // 文字化け防止。「UTF-8だよー」
+    const blob = new Blob([bom,csv], {type: "text/csv;charset=utf-8"});
+    const date = new Date();
+    const timestamp =
+      date.getFullYear() +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      ("0" + date.getDate()).slice(-2) +
+      "_" +
+      ("0" + date.getHours()).slice(-2) +
+      ("0" + date.getMinutes()).slice(-2) +
+      ("0" + date.getSeconds()).slice(-2);
+    const fileName = `click_data_${timestamp}.csv`;
+    const link  = document.createElement("a"); // 謎　調べる
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    setTimeout(() => {
+      URL.revokeObjectURL(link.href);
+    },100);
   };
+
 
 
 
@@ -82,7 +134,7 @@ function App() {
                 <th>No.</th>
                 <th>X座標</th>
                 <th>座標</th>
-                <th>時間</th>
+                <th>時間(sec)</th>
                 <th>補足</th>
                 <th>操作</th>
               </tr>
